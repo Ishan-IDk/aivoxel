3099ce8746cbcb3c8b8a6c411f0ff6904d1ac507
Ishan Singh <70334477+Ishan-IDk@users.noreply.github.com>
2024-08-29 18:38:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var typing=new Typed(".text", {
+    strings: ["Reimagined.", "Redefined.", "Revolutionized."],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true,
+});
+
+// gsap.fromTo(
+//     ".page",
+//     {display: "none",},
+//     {
+//       opacity: 1,
+//       display: "",
+//       duration: 1.4,
+//       delay: 3.5,
+//     }
+//   );
+//   gsap.fromTo(
+//     ".loader",
+//     { opacity: 1 },
+//     {
+//       opacity: 0,
+//       display: "none",
+//       duration: 1,
+//       delay: 2.5,
+//     }
+//   );
